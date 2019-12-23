52e2e4d2cea655b3510bfbce31fe6648be091b52
User1 <sujalshaikhsk@gmail.com>
2019-12-23 12:58:40 +0530

    customer entity added

---
+++ src/main/java/com/strickers/shoppingcartapp/entity/Customer.java
@@ -0,0 +1,35 @@
+package com.strickers.shoppingcartapp.entity;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customer")
+@Setter
+@Getter
+public class Customer implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long customerId;
+
+	private String customerName;
+	private Long mobileNumber;
+	private String address;
+	private String gender;
+	private LocalDate dateOfBirth;
+	private Double salary;
+	private String email;
+	private String password;
+}
